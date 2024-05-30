342f8fa6aef9f51e644b32a979ccd91ed20cfc53
Elsabordeaux <snowpaws85@gmail.com>
2024-05-30 19:36:54 +0100

    Added basic javascript date and time functions using moment

---
+++ script.js
@@ -0,0 +1,55 @@
+function updateLATime() {
+  let losAngeles = document.querySelector("#los-angeles");
+  let losAngelesDate = losAngeles.querySelector(".date");
+  let losAngelesTime = losAngeles.querySelector(".time");
+
+  losAngelesDate.innerHTML = moment().format(" MMMM Do YYYY");
+  losAngelesTime.innerHTML = moment()
+    .tz("America/Los_Angeles")
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateLATime();
+setInterval(updateLATime, 1000);
+
+function updateSydneyTime() {
+  let sydney = document.querySelector("#sydney");
+  let sydneyDate = sydney.querySelector(".date");
+  let sydneyTime = sydney.querySelector(".time");
+
+  sydneyDate.innerHTML = moment().format(" MMMM Do YYYY");
+  sydneyTime.innerHTML = moment()
+    .tz("Australia/Sydney")
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateSydneyTime();
+setInterval(updateSydneyTime, 1000);
+
+function updateTokyoTime() {
+  let tokyo = document.querySelector("#tokyo");
+  let tokyoDate = tokyo.querySelector(".date");
+  let tokyoTime = tokyo.querySelector(".time");
+
+  tokyoDate.innerHTML = moment().format(" MMMM Do YYYY");
+  tokyoTime.innerHTML = moment()
+    .tz("Asia/Tokyo")
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTokyoTime();
+setInterval(updateTokyoTime, 1000);
+
+function updateParisTime() {
+  let paris = document.querySelector("#paris");
+  let parisDate = paris.querySelector(".date");
+  let parisTime = paris.querySelector(".time");
+
+  parisDate.innerHTML = moment().format(" MMMM Do YYYY");
+  parisTime.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateParisTime();
+setInterval(updateParisTime, 1000);
